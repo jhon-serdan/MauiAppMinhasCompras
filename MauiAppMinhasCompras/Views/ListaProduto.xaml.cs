106bbd937ef8d0d4ebc2547856611d439a4a4555
Jonthan <jonatanm@live.com>
2025-03-16 17:32:30 -0300

    implementação da busca dinâmica com o collection

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,29 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
+
+
 
 public partial class ListaProduto : ContentPage
 {
+
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
 	public ListaProduto()
 	{
 		InitializeComponent();
-	}
+		
+		col_produtos.ItemsSource = lista;
+
+    }
+
+    protected async override void OnAppearing()
+    {
+		List<Produto> tmp = await App.DB.GetAll();
+
+		tmp.ForEach(i => lista.Add(i));
+    }
 
     private async void Button_Novo_Produto(object sender, EventArgs e)
     {
@@ -18,5 +36,25 @@ public partial class ListaProduto : ContentPage
 			await DisplayAlert("Ops", ex.Message, "OK");
 		}
 
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App.DB.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:C}";
+
+		DisplayAlert("Toal dos Produtos", msg, "ok");
     }
 }
