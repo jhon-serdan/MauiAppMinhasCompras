a9cc1660d4b493508e97dc05bd59fa1753473e82
Jonthan <jonatanm@live.com>
2025-03-23 23:53:53 -0300

    OK

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,9 +1,40 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class EditarProduto : ContentPage
 {
-	public EditarProduto()
-	{
-		InitializeComponent();
-	}
+    public EditarProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked_Salvar_Alteracao(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.DB.update(p);
+            await DisplayAlert("Sucesso", "Registro Alterado", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void Button_Clicked_Voltar_Home(object sender, EventArgs e)
+    {
+        await Navigation.PopToRootAsync();
+    }
 }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,5 +1,6 @@
 using MauiAppMinhasCompras.Models;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace MauiAppMinhasCompras.Views;
 
@@ -20,12 +21,21 @@ public partial class ListaProduto : ContentPage
 
     protected async override void OnAppearing()
     {
-		lista.Clear();
+		try
+		{
+			lista.Clear();
 
-        List<Produto> tmp = await App.DB.GetAll();
+			List<Produto> tmp = await App.DB.GetAll();
 
-		tmp.ForEach(i => lista.Add(i));
+			tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+	    {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+
     }
+
 
     private async void Button_Novo_Produto(object sender, EventArgs e)
     {
@@ -42,16 +52,24 @@ public partial class ListaProduto : ContentPage
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-		string q = e.NewTextValue;
+		try
+		{
 
-		lista.Clear();
+			string q = e.NewTextValue;
 
-        List<Produto> tmp = await App.DB.Search(q);
+			lista.Clear();
 
-        tmp.ForEach(i => lista.Add(i));
+			List<Produto> tmp = await App.DB.Search(q);
+
+			tmp.ForEach(i => lista.Add(i));
+
+        } catch (Exception ex)
+		{
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private void Button_Clicked_Somar(object sender, EventArgs e)
     {
 		double soma = lista.Sum(i => i.Total);
 
@@ -59,4 +77,49 @@ public partial class ListaProduto : ContentPage
 
 		DisplayAlert("Toal dos Produtos", msg, "ok");
     }
+
+    private async void SwipeItem_Remover(object sender, EventArgs e)
+    {
+		try
+		{
+			SwipeItem selecionado = sender as SwipeItem;
+
+			Produto p = selecionado?.BindingContext as Produto;
+
+			bool confirm = await DisplayAlert("Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
+
+            if (confirm)
+			{
+                await App.DB.delete(p.Id);
+                lista.Remove(p);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void SwipeItem_Editar(object sender, EventArgs e)
+    {
+        try
+        {
+            SwipeItem selecionado = sender as SwipeItem;
+            Produto p = selecionado?.BindingContext as Produto;
+
+            if (p != null)
+            {
+                await Navigation.PushAsync(new Views.EditarProduto
+                {
+                    BindingContext = p
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
 }
